96f5a5656bc2be0cf4534c88941551b495944a12
Tim Obezuk <tobezuk@gmail.com>
2013-11-17 19:07:17 +1000

    websocket and express framework

---
+++ app.js
@@ -0,0 +1,49 @@
+  express = require('express');
+
+  app = express();
+
+  http = require('http');
+
+  app.configure(function() {
+
+    app.set('port', process.env.PORT || 3000);
+    // app.set('views', __dirname + '/views');
+    // app.set('view engine', 'ejs');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+
+    app.use(express.cookieParser());
+
+    app.use(app.router);
+
+    path = require('path');
+    app.use(express.static(path.join(__dirname, 'public')));
+
+  });
+
+  app.configure('development', function(){
+    app.use(express.errorHandler());
+  });
+
+  server = http.createServer(app).listen(app.get('port'), function() {
+
+    console.log("Express server listening on port " + app.get('port'));
+
+    console.log('Running Node Version: ' + process.version);
+  });
+
+  io = require('socket.io').listen(server);
+
+  io.set('log level', 0);
+
+  io.on('connection', function (client) {
+
+  	// Do Login Event
+
+  	client.on('disconnect', function() {
+  		// Do Disconnect Event
+      });
+
+  });
